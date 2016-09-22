9b2d9dee96aba33c96905b16c40ad7daccf6b336
andela-ggikera <githinji.gikera@andela.com>
2016-09-22 11:49:26 +0300

    (feature) Add sharing capability in details activity

--- app/src/main/java/com/example/android/forecast/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/forecast/DetailActivityFragment.java
@@ -3,6 +3,8 @@ package com.example.android.forecast;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,6 +17,11 @@ import android.widget.TextView;
  * A placeholder fragment containing a simple view.
  */
 public class  DetailActivityFragment extends Fragment {
+
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = "ForecastApp";
+    private String forecastString;
 
     public DetailActivityFragment() {
     }
@@ -32,17 +39,42 @@ public class  DetailActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String forecastString = intent.getStringExtra(Intent.EXTRA_TEXT);
-            ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastString);
+            forecastString = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.detail_text))
+                    .setText(forecastString);
         }
 
         return rootView;
+
+    }
+
+    private Intent shareForecastIntent () {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        // prevent the activity from being placed into the activity stack
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,
+                forecastString + FORECAST_SHARE_HASHTAG);
+
+        return intent;
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        inflater.inflate(R.menu.menu_settings, menu);
+        inflater.inflate(R.menu.menu_detail_fragment, menu);
+
+        // Get the shared menu item
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // Get the provider to set the share intent
+        ShareActionProvider shareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // Attach intent to this shared action provider
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(shareForecastIntent());
+        }
     }
 
     @Override
