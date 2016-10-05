debc8714c3261601299a2a8786104405ed221501
Jee Githinji Gikera <githinji.gikera@andela.com>
2016-10-05 12:59:40 +0300

    (chore) Create a sync service to bind the sync adapter binder

--- app/src/main/java/com/example/android/forecast/sync/ForecastSyncAdapter.java
+++ app/src/main/java/com/example/android/forecast/sync/ForecastSyncAdapter.java
@@ -1,10 +1,23 @@
 package com.example.android.forecast.sync;
 
+import android.accounts.Account;
 import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
 
 /**
  * Created by nerd on 05/10/2016.
  */
 
 public class ForecastSyncAdapter extends AbstractThreadedSyncAdapter {
+    public ForecastSyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+
+    }
 }
--- app/src/main/java/com/example/android/forecast/sync/ForecastSyncService.java
+++ app/src/main/java/com/example/android/forecast/sync/ForecastSyncService.java
@@ -1,10 +1,31 @@
 package com.example.android.forecast.sync;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 
 /**
  * Created by nerd on 05/10/2016.
  */
 
 public class ForecastSyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static ForecastSyncAdapter sForecastSyncAdapter = null;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sForecastSyncAdapter.getSyncAdapterBinder();
+    }
+
+    @Override
+    public void onCreate() {
+        synchronized (sSyncAdapterLock) {
+            if (sForecastSyncAdapter == null) {
+                sForecastSyncAdapter = new ForecastSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
 }
