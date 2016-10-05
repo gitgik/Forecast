1545184ec6db67eaa380c0be3cb0e725bbdbbf46
Jee Githinji Gikera <githinji.gikera@andela.com>
2016-10-05 12:53:11 +0300

    (chore) Define xml for authenticator service, add permissions for the sync adapter

---
+++ app/src/main/java/com/example/android/forecast/sync/ForecastSyncAdapter.java
@@ -0,0 +1,10 @@
+package com.example.android.forecast.sync;
+
+import android.content.AbstractThreadedSyncAdapter;
+
+/**
+ * Created by nerd on 05/10/2016.
+ */
+
+public class ForecastSyncAdapter extends AbstractThreadedSyncAdapter {
+}
---
+++ app/src/main/java/com/example/android/forecast/sync/ForecastSyncService.java
@@ -0,0 +1,10 @@
+package com.example.android.forecast.sync;
+
+import android.app.Service;
+
+/**
+ * Created by nerd on 05/10/2016.
+ */
+
+public class ForecastSyncService extends Service {
+}
