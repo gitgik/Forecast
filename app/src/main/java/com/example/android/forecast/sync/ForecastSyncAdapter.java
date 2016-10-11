cad99680ce70f70dd072b9fc47b3472e70e633c9
Jee Githinji Gikera <githinji.gikera@andela.com>
2016-10-11 11:16:05 +0300

    (chore) Define an annotation to determine the location status of the server

--- app/src/main/java/com/example/android/forecast/sync/ForecastSyncAdapter.java
+++ app/src/main/java/com/example/android/forecast/sync/ForecastSyncAdapter.java
@@ -20,6 +20,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.IntDef;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
@@ -38,6 +39,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Calendar;
@@ -63,6 +66,7 @@ public class ForecastSyncAdapter extends AbstractThreadedSyncAdapter {
             WeatherEntry.COLUMN_SHORT_DESC
     };
 
+
     // These indices must mach the projection
     private static final int INDEX_WEATHER_ID = 0;
     private static final int INDEX_MAX_TEMP = 1;
@@ -71,6 +75,16 @@ public class ForecastSyncAdapter extends AbstractThreadedSyncAdapter {
 
     private static final long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
     private static final int WEATHER_NOTIFICATION_ID = 3004;
+
+    @Retention(RetentionPolicy.SOURCE) // Retain only for our source code
+    @IntDef({LOCATION_STATUS_OK, LOCATION_STATUS_SERVER_DOWN, LOCATION_STATUS_SERVER_INVALID,
+    LOCATION_STATUS_UNKNOWN})
+    public @interface LocationStatus {}
+
+    public static final int LOCATION_STATUS_OK = 0;
+    public static final int LOCATION_STATUS_SERVER_DOWN = 1;
+    public static final int LOCATION_STATUS_SERVER_INVALID = 2;
+    public static final int LOCATION_STATUS_UNKNOWN = 3;
 
     private boolean DEBUG = true;
 
